b9f5fbe961edfcf1ccf89cc6f3d8bfb5dc780882
ArcFarM <hinsung99@gmail.com>
2024-11-28 18:12:01 +0900

    asdf

--- Assets/Trickcal/Scripts/Enemy_Manager.cs
+++ Assets/Trickcal/Scripts/Enemy_Manager.cs
@@ -1,14 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy_Manager : MonoBehaviour
 {
-    //적의 스탯을 생성 및 관리
-
-    
-
+    //적의 스탯을 관리
     public GameObject enemy;
+
+    //---------------------------적 스탯----------------------------
+    //id
+    int id;
+    //체력
+    double hp;
+    //클래스(일반/특수/보스)
+    enum Enemy_Class { Normal, Special, Boss };
+    Enemy_Class ec;
+    //특수 적 한정) 사용할 스펠
+    enum Sp_Enemy_Spell { SpeedUp, HPRegen, DmgOne };
+    Sp_Enemy_Spell? es = null;
+    //이동 속도
+    float spd;
+    //방어력
+    int armor;
+
+    private void Set_Values()
+    {
+
+    }
+
     private void Start()
     {
        //싱글톤에 있는 적 생성 메서드 사용
@@ -19,4 +39,29 @@ public class Enemy_Manager : MonoBehaviour
     {
         
     }
+
+    //wav : 현재 웨이브 번호 flag : 특수 적 소환 여부
+    public GameObject Enemy_Setting(int wav, bool flag)
+    {
+        GameObject result = new GameObject();
+        result.GetComponent<Enemy_Manager>();
+        //보스 소환
+        if(wav %5 == 0)
+        {
+            
+        }
+        //특수 미션 소환
+        else if (flag)
+        {
+
+        }
+        //일반 적 소환
+        else
+        {
+
+        }
+        return result;
+    }
+
+    
 }
--- Assets/Trickcal/Scripts/GameManager.cs
+++ Assets/Trickcal/Scripts/GameManager.cs
@@ -29,19 +29,7 @@ public class GameManager : MonoBehaviour
     int coupon;
     //현재 스테이지
     int wave;
-    //---------------------------적 스탯----------------------------
-    //id
-    int id;
-    //체력
-    double hp;
-    //클래스(일반/특수/보스)
-    enum Enemy_Class { Normal, Special, Boss };
-    //특수 적 한정) 사용할 스펠
-    enum Sp_Enemy_Spell { SpeedUp, HPRegen, DmgOne };
-    //이동 속도
-    float spd;
-    //방어력
-    int armor;
+
 
     //적 생성과 타겟팅, 제거를 담당할 전역 리스트
     private List<GameObject> Enemys = new List<GameObject>();
@@ -52,6 +40,8 @@ public class GameManager : MonoBehaviour
         //null check
         if (go == null || go.tag != "Enemy") return;
 
+        //Enemy_Manager를 통해 세팅
+        
         //전역 리스트에 추가
         Enemys.Add(go);
         go.GetComponent<Enemy_Manager>();
