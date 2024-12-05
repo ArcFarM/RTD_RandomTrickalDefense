1a3c52c729e2437225a88763ce0eab1e69ca9b51
harapia <iromi10924@gmail.com>
2024-12-05 19:42:11 +0900

    적 생성 버튼 스크립트 구현

--- Assets/Trickcal/Scripts/GameManager.cs
+++ Assets/Trickcal/Scripts/GameManager.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Button Enemy_Spawn_button;
+    private IEnumerator coroutine;
     // Start is called before the first frame update
     private static GameManager instance;
 
@@ -34,6 +37,22 @@ public class GameManager : MonoBehaviour
     //적 생성과 타겟팅, 제거를 담당할 전역 리스트
     private List<GameObject> Enemys = new List<GameObject>();
 
+    public void Spawn_Enemy_Button(){
+        coroutine = CountDownStart(10);
+        StartCoroutine(coroutine);
+        return;
+    }
+
+
+    IEnumerator CountDownStart(int CountDown)
+    {
+        while (CountDown > 0)
+        {//카운트다운이 0이 될때까지
+            CountDown--;//카운트다운 감소
+            Create_Enemy();
+            yield return new WaitForSeconds(1.0f);//1초대기
+        }
+    }
     //적 생성
     public void Create_Enemy()
     {
@@ -69,6 +88,9 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    void Start(){
+        Enemy_Spawn_button.onClick.AddListener(Spawn_Enemy_Button);
+    }
     // Update is called once per frame
     void Update()
     {
